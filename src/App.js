e3153fc750707d0368e2ce553310990f3e6a1b7f
GarmaNazareno <garmatorresnazareno@gmail.com>
2022-04-26 05:20:13 -0300

    homepage

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ function App() {
           <Routes>
            
             <Route path="/carvel-ecommerce" element={<ItemListContainer />} />
-            <Route path="/category/:id" element={<ItemList />} />
-            <Route path="/item/:id" element={<ItemDetailContainer />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route path="/carvel-ecommerce/category/:id" element={<ItemList />} />
+            <Route path="/carvel-ecommerce/item/:id" element={<ItemDetailContainer />} />
+            <Route path="/carvel-ecommerce/cart" element={<Cart />} />
             <Route path="*" element={<NotFound404 />} />
           </Routes>
         </div>
